e5b88febee385c3ede799629ec98fa845b1e1ac5
Osamu Ito <mr.samito@gmail.com>
2022-08-31 13:17:55 -0400

    commit tree eec089d0e1c045ee88b1efb24e6947582b09121c

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+
+
+
+//what is the this saying simple
+//-store each employee as an Object.
+//assign employye variable to an object containing name and streetAddress keys.
+
+const employee = {
+    name: "Sam Ito",
+    streetAddress: "101 Jefferson Avenue",
+} 
+
+function updateEmployeeWithKeyAndValue(employees,key,value) {
+
+    const newEmployee = {...employees};
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employees,key,value) {
+
+   employees[key] = value;
+
+    return employee;
+}
+
+//call back to something non existent in the function.
+function deleteFromEmployeeByKey(employee, key) {
+
+    const cloneEmployee = {...employee}
+    delete cloneEmployee.name;
+    return cloneEmployee;
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
